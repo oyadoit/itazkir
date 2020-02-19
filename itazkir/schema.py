ebbd020accb1c3463cb754f4ba6f24d370c9246d
Jubril Issa <issa.jubril@andela.com>
2020-02-19 18:51:41 +0100

    add basic query schema for reminder

--- itazkir/schema.py
+++ itazkir/schema.py
@@ -1,11 +1,13 @@
 import graphene
 import graphql_jwt
+from graphene_django.debug import DjangoDebug
 
 import accounts.schema
+import reminder.schema
 
 
-class Query(accounts.schema.Query, graphene.ObjectType):
-    pass
+class Query(accounts.schema.Query, reminder.schema.Query, graphene.ObjectType):
+    debug = graphene.Field(DjangoDebug, name="_debug")
 
 class Mutation(accounts.schema.Mutation, graphene.ObjectType):
     pass
--- reminder/schema.py
+++ reminder/schema.py
@@ -10,3 +10,16 @@ class ReminderType(DjangoObjectType):
         model = Reminder
 
 
+class Query(graphene.ObjectType):
+    all_reminders = graphene.List(ReminderType, description="All the Reminder present")
+    reminder = graphene.Field(ReminderType, id=graphene.Int(), name=graphene.String(), description="Single reminder")
+
+    # user_reminders = graphene.List(ReminderType, owner=)
+    # user_reminder = graphene.Field(ReminderType, id=graphene.Int(), )
+
+    def resolve_all_reminders(self, info, **kwargs):
+        return Reminder.objects.all()
+    
+    def resolve_reminder(self, info, id=None):
+        if id is not None:
+            return Reminder.objects.get(pk=id)
