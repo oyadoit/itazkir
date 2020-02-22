dbaa24ed9a7582f0a1ff67228e6ca60e27b20b99
Jubril Issa <issa.jubril@andela.com>
2020-02-22 23:33:03 +0100

    Add content to the itazkir schema

--- itazkir/schema.py
+++ itazkir/schema.py
@@ -4,12 +4,13 @@ from graphene_django.debug import DjangoDebug
 
 import accounts.schema
 import reminder.schema
+import content.schema
 
 
-class Query(accounts.schema.Query, reminder.schema.Query, graphene.ObjectType):
+class Query(accounts.schema.Query, reminder.schema.Query, content.schema.Query, graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name="_debug")
 
-class Mutation(accounts.schema.Mutation, reminder.schema.Mutation, graphene.ObjectType):
+class Mutation(accounts.schema.Mutation, reminder.schema.Mutation, content.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
