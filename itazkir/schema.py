37379a6b50a89f78f7d1be418f1ff9f3786f6833
Jubril Issa <issa.jubril@andela.com>
2020-02-25 09:37:48 +0100

    Add subscription schema to the root schema and format file

--- itazkir/schema.py
+++ itazkir/schema.py
@@ -5,12 +5,26 @@ from graphene_django.debug import DjangoDebug
 import accounts.schema
 import reminder.schema
 import content.schema
+import subscription.schema
 
 
-class Query(accounts.schema.Query, reminder.schema.Query, content.schema.Query, graphene.ObjectType):
+class Query(
+        accounts.schema.Query,
+        reminder.schema.Query,
+        content.schema.Query,
+        subscription.schema.Query,
+        graphene.ObjectType
+):
     debug = graphene.Field(DjangoDebug, name="_debug")
 
-class Mutation(accounts.schema.Mutation, reminder.schema.Mutation, content.schema.Mutation, graphene.ObjectType):
+
+class Mutation(
+        accounts.schema.Mutation,
+        reminder.schema.Mutation,
+        content.schema.Mutation,
+        subscription.schema.Mutation,
+        graphene.ObjectType
+):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
