a820e5687ad39d9fa956287ecd6514912a5f15f9
Jubril Issa <issa.jubril@andela.com>
2020-02-25 09:32:03 +0100

    Add subscription app to the project

---
+++ subscription/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.conf import settings
+
+class Subscription(models.Model):
+    reminder = models.ForeignKey('reminder.Reminder', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
