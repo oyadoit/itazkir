2c60f46698e023fbe620adffa42e6c5f60248f30
Jubril Issa <issa.jubril@andela.com>
2020-05-05 09:17:47 +0100

    Enforce unique constraint for user subscription

--- subscription/models.py
+++ subscription/models.py
@@ -4,3 +4,6 @@ from django.conf import settings
 class Subscription(models.Model):
     reminder = models.ForeignKey('reminder.Reminder', on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [['reminder', 'user']]
