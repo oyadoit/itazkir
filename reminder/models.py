4d6370699df9566c25c677cf476617c93a288694
Jubril Issa <jubrilissa@gmail.com>
2020-06-27 09:44:45 +0100

    Add is_approved flag for reminders

--- reminder/models.py
+++ reminder/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+
 class Reminder(models.Model):
+    is_approved = models.BooleanField(default=False)
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
