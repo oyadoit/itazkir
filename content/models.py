f6acaf43ec97de9ff047d609c4380b4893139387
Jubril Issa <jubrilissa@gmail.com>
2020-06-27 09:42:13 +0100

    Add content image to model and add migration

--- content/models.py
+++ content/models.py
@@ -7,3 +7,4 @@ class Content(models.Model):
     reminder = models.ForeignKey('reminder.Reminder', on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     data = models.TextField()
+    content_image = models.CharField(max_length=200, null=True)
