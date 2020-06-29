22b3d6a0e893ab23329035272273b2699d285b8a
Jubril Issa <jubrilissa@gmail.com>
2020-06-29 23:31:06 +0100

    Add models for favorite content

--- favourite/models.py
+++ favourite/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Favourite(models.Model):
+    content = models.ForeignKey('content.Content', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
